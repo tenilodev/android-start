333a970e2aca8a9e7ae45ffb507f2a4faae2afb6
aisatriani <aisatriani03@gmail.com>
2018-04-23 20:47:24 +0800

    add pojo class and modify extensions

--- app/src/main/java/com/tenilodev/androidstarter/ui/main/MainActivity.kt
+++ app/src/main/java/com/tenilodev/androidstarter/ui/main/MainActivity.kt
@@ -5,16 +5,10 @@ import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import com.tenilodev.androidstarter.R
-import com.tenilodev.androidstarter.pojo.Resource
-import com.tenilodev.androidstarter.pojo.Status
 import com.tenilodev.androidstarter.ui.base.BaseActivity
-import com.tenilodev.androidstarter.utils.addOnResultListener
-import com.tenilodev.androidstarter.utils.displaySnakbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -37,8 +31,6 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 
         nav_view.setNavigationItemSelectedListener(this)
 
-        val res = Resource.success("mantapkan")
-        res.ad
     }
 
     override fun onBackPressed() {
